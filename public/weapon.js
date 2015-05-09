f9a278bec0dce8b97d25cb7489da9e5191f55173
JC Situ <situjc@gmail.com>
2015-05-09 23:10:18 +0800

    Added start & end game, bonus effects, weapon upgrade, sound effects

--- public/weapon.js
+++ public/weapon.js
@@ -30,6 +30,8 @@ Bullet.prototype.fire = function (x, y, angle, speed, gx, gy) {
     this.angle = angle;
 
     this.body.gravity.set(gx, gy);
+
+    game.sound.play('fire');
 };
 
 Bullet.prototype.update = function () {
@@ -91,4 +93,42 @@ Weapon.Rockets.prototype.fire = function (source) {
     this.nextFire = this.game.time.time + this.fireRate;
 };
 
+Weapon.RocketsX3 = function (game) {
+    Phaser.Group.call(this, game, game.world, 'RocketsX3', false, true, Phaser.Physics.ARCADE);
 
+    this.nextFire = 0;
+    this.bulletSpeed = 400;
+    this.fireRate = 1000;
+
+    for (var i = 0; i < 32; i++) {
+        this.add(new Bullet(game, 'bullet10'), true);
+    }
+
+    this.setAll('tracking', true);
+
+    return this;
+};
+
+Weapon.RocketsX3.prototype = Object.create(Phaser.Group.prototype);
+Weapon.RocketsX3.prototype.constructor = Weapon.RocketsX3;
+
+Weapon.RocketsX3.prototype.fire = function (source) {
+    if (this.game.time.time < this.nextFire) { return; }
+
+    var x = source.x + 10;
+    var y = source.y + 10;
+
+    // control rocket directions
+    if(player.faceDirection == "left") {
+        this.getFirstExists(false).fire(x, y, -90, this.bulletSpeed, -500, 300);    // up
+        this.getFirstExists(false).fire(x, y, -170, this.bulletSpeed, -500, 300);   // left
+        this.getFirstExists(false).fire(x, y, 90, this.bulletSpeed, -500, 300);     // down
+    }
+    else if(player.faceDirection == "right") {
+        this.getFirstExists(false).fire(x, y, -90, this.bulletSpeed, 500, 300);     // up
+        this.getFirstExists(false).fire(x, y, -10, this.bulletSpeed, 500, 300);     // right
+        this.getFirstExists(false).fire(x, y, 90, this.bulletSpeed, 500, 300);      // down
+    }
+        
+    this.nextFire = this.game.time.time + this.fireRate;
+};
